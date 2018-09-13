10b18acdf12def309fed5f497c8566a62238cc79
David Leal <leal.davidx@gmail.com>
2018-09-13 17:07:31 -0600

    db

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: '127.0.0.1:3306',
+  host: '127.0.0.1',
   user: 'root',
   password: '1david',
   database: 'easy_credit'
