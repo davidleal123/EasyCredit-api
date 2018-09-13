130901438982cd7c780ac77839244fbc30ac0246
David Leal <leal.davidx@gmail.com>
2018-09-13 00:11:19 -0600

    Connection Closing

--- controllers/connection.js
+++ controllers/connection.js
@@ -6,8 +6,16 @@ const connection = dbconn.connection;
 
 function getPrueba(req, res) {
     connection.connect((err) => {
-        if (err) throw err;
+        if (err) {
+            throw err;
+        }
+    
         console.log('Connected!');
+        connection.end((err) => {
+            // The connection is terminated gracefully
+            // Ensures all previously enqueued queries are still
+            // before sending a COM_QUIT packet to the MySQL server.
+          });
         res.status(200).send({mensaje: "conectado"});
       });
 }
